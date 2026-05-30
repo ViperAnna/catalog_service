7e3a9e35dc0464b07771bbb79af717411067862d
ViperAnna <122033767+ViperAnna@users.noreply.github.com>
2026-05-30 15:04:33 +0300

    Revert "Merge pull request #38 from ViperAnna/feature/SAM1-14"
    This reverts commit c5e51e9a782d06f3ae8301572c3707abe0cd956d, reversing
    changes made to 9669a91227a52458f6a469b55c660b83170c1269.
    
    The revrtts commit

--- backend/src/main/java/ru/klimovich/catalog_service/controller/CategoryController.java
+++ backend/src/main/java/ru/klimovich/catalog_service/controller/CategoryController.java
@@ -4,16 +4,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.klimovich.catalog_service.dto.Response;
 import ru.klimovich.catalog_service.dto.request.CategoryRequest;
 import ru.klimovich.catalog_service.dto.response.CategoryResponse;
-import ru.klimovich.catalog_service.dto.validation.OnCreate;
-import ru.klimovich.catalog_service.dto.validation.OnUpdate;
 import ru.klimovich.catalog_service.service.impl.CategoryServiceImpl;
 
 import java.time.LocalDateTime;
@@ -34,7 +32,7 @@ public class CategoryController {
     @ApiResponse(responseCode = "201", description = "Категория успешно создана")
     @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public Response createCategory(@Validated(OnCreate.class) @ModelAttribute CategoryRequest categoryRequest) {
+    public Response createCategory(@Valid @ModelAttribute CategoryRequest categoryRequest) {
         categoryService.createCategory(categoryRequest);
         return new Response(
                 CATEGORY_CREATED_SUCCESSFULLY,
@@ -68,7 +66,7 @@ public class CategoryController {
             @ApiResponse(responseCode = "404", description = "Категория не найдена")
     })
     @PutMapping("/{id}")
-    public Response updateCategory(@PathVariable String id, @Validated(OnUpdate.class) @ModelAttribute CategoryRequest categoryDetails) {
+    public Response updateCategory(@PathVariable String id, @Valid @ModelAttribute CategoryRequest categoryDetails) {
         categoryService.updateCategoryById(id, categoryDetails);
         return new Response(
                 CATEGORY_UPDATE_SUCCESSFULLY,
--- backend/src/main/java/ru/klimovich/catalog_service/dto/request/CategoryRequest.java
+++ backend/src/main/java/ru/klimovich/catalog_service/dto/request/CategoryRequest.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.web.multipart.MultipartFile;
-import ru.klimovich.catalog_service.dto.validation.OnCreate;
 import ru.klimovich.catalog_service.util.imageUtils.ValidImage;
 
 @Getter
@@ -28,7 +27,7 @@ public class CategoryRequest {
     @Schema(description = "Описание категории", example = "Фотокамеры, дроны, видеокамеры")
     private String description;
 
-    @NotNull(message = "Image is required.", groups = OnCreate.class)
+    @NotNull(message = "Image is required.")
     @ValidImage(allowedTypes = {"image/jpeg", "image/jpg", "image/pjpeg", "application/octet-stream"})
     @Schema(description = "Картинка категории", example = "image/categories/digital_devices.jpg")
     private MultipartFile image;
