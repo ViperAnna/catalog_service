c2f93a471a8fb1edd766d95f09089ffc7a966d9e
ViperAnna <122033767+ViperAnna@users.noreply.github.com>
2026-07-06 18:31:08 +0300

    moved catalog_service

--- catalog-service/src/main/java/ru/klimovich/catalog_service/CatalogServiceApplication.java
+++ catalog-service/src/main/java/ru/klimovich/catalog_service/CatalogServiceApplication.java
@@ -2,10 +2,12 @@ package ru.klimovich.catalog_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableDiscoveryClient
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
